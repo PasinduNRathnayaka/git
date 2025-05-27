3b9337fa5b735e1c0636a203d0f8eef5fd051fb9
Pasindu Niroshan Rathnayaka <rathnayaka12106@usci.ruh.ac.lk>
2025-05-27 10:42:47 +0530

    add address

--- PrintMyName.java
+++ PrintMyName.java
@@ -2,5 +2,6 @@ public class PrintMyName {
     public static void main(String[] args) {
         System.out.println("Pasindu Niroshan Rathnayaka");
          System.out.println("MY university is UOR");
+         System.out.println("MY address is Anuradhapura");
     }
 }
