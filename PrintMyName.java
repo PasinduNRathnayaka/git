211da50b44e43cb1f73361868dbc17551433023c
Pasindu Niroshan Rathnayaka <rathnayaka12106@usci.ruh.ac.lk>
2025-05-27 11:11:47 +0530

    add index number

--- PrintMyName.java
+++ PrintMyName.java
@@ -3,5 +3,6 @@ public class PrintMyName {
         System.out.println("Pasindu Niroshan Rathnayaka");
          System.out.println("MY university is UOR");
          System.out.println("MY address is Anuradhapura");
+         System.out.println("MY index number SC/2021/12106");
     }
 }
